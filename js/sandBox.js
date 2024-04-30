2ee0746617578aa358ad1e3358efd7d17dadbe1b
Benja-san <beugnet.freelance@gmail.com>
2024-04-30 15:13:32 +0200

    add tsconfig and specific folders add index.html for web display

--- js/sandBox.js
+++ js/sandBox.js
@@ -1,14 +1,15 @@
-var messageText = "Hello Bob!";
-var messageCreatedAt = new Date();
+"use strict";
+const messageText = "Hello Bob!";
+const messageCreatedAt = new Date();
 console.log(messageCreatedAt);
 function stringifyMessage(text, createdAt) {
-    return "".concat(text, ", the ").concat(createdAt.toLocaleDateString());
+    return `${text}, the ${createdAt.toLocaleDateString()}`;
 }
 console.log(stringifyMessage(messageText, messageCreatedAt));
-var message1 = { from: "Bob", message: "Hello Alice!", isRead: true };
-var message2 = { from: "Alice", message: "Hi Bob!" };
-var notification1 = { at: 1694011133, state: "unread" };
-var notification2 = { at: 1694011532, state: "read" };
+const message1 = { from: "Bob", message: "Hello Alice!", isRead: true };
+const message2 = { from: "Alice", message: "Hi Bob!" };
+const notification1 = { at: 1694011133, state: "unread" };
+const notification2 = { at: 1694011532, state: "read" };
 function readNotificationOrMessage(notificationOrMessage) {
     if ("state" in notificationOrMessage) {
         notificationOrMessage.state = "read";
